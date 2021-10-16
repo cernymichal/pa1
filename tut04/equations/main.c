aa1ae4283c03632512343f87bf3a6dfa14751350
Michal Černý <33428111+cernymichal@users.noreply.github.com>
2021-10-16 15:45:15 +0200

    tut04: Add input validation

--- tut04/equations/main.c
+++ tut04/equations/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main (void) {
+    printf("Zadejte rovnici:\n");
+
+    double a, b, c;
+    char op[2];
+
+    if (scanf(" %lf %1[+-*/] %lf = %lf", &a, op, &b, &c) != 4 || (op[0] == '/' && b == 0)) {
+        printf("Nespravny vstup.\n");
+        return 1;
+    }
+
     return 0;
 }
 
