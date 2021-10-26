3161d4f68983d39ac03d1505271b4c97fe601b22
Michal Černý <33428111+cernymichal@users.noreply.github.com>
2021-10-26 12:26:56 +0200

    hw02: Add solution

--- hw02/num_series/main.c
+++ hw02/num_series/main.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-#include <math.h>
-
-#define RESULT_LENGTH 128
 
 int clear_stdin(void) {
     while (true) {
@@ -15,40 +12,83 @@ int clear_stdin(void) {
     return 0;
 }
 
+long long powll(long long a, int n) {
+    long long result = 1;
+
+    for (int i = 1; i <= n; i *= 2) {
+        if (n & i)
+            result *= a;
+        a *= a;
+    }
+
+    return result;
+}
+
 int print_incorrect_input(void) {
     printf("Nespravny vstup.\n");
     return 1;
 }
 
-void print_result(int result[], int digit) {
-    for (int i = 0; i < RESULT_LENGTH; i++) {
-        if (result[i] == -1)
-            break;
+void print_result(long long result, int base, int digit) {
+    int result_length = 32 - (base - 2);
+    int skip = 1;
+    for (int i = 0; i < result_length; i++) {
+        long long p = powll(base, result_length - i - 1);
+        int number = result / p;
+        result -= number * p;
 
-        char c = result[i] + 48;
+        if (skip) {
+            if (!number && i < result_length - 1)
+                continue;
+            else
+                skip = 0;
+        }
+
+        char c = number + 48;
         c += c > 57 ? 39 : 0;
         printf("%c", c);
     }
 
     printf("\n");
 
-    for (int i = 1; i < digit; i++)
+    for (int i = 0; i < digit; i++)
         printf(" ");
 
     printf("^\n");
 }
 
-int input_pos(long long *pos, int *sys) {
-    if (scanf(" %lld %d", pos, sys) != 2 || *pos < 0 || *sys < 2 || *sys > 36)
+int input_pos(long long *pos, int *base) {
+    if (scanf(" %lld %d", pos, base) != 2 || *pos < 0 || *base < 2 || *base > 36)
         return 1;
 
     return 0;
 }
 
-void solve(long long pos, int sys, int result[], int *digit) {
-    *digit = 1;
-    result[0] = 1;
-    result[1] = -1;
+void solve(long long pos, int base, long long *result, int *digit) {
+    *result = 0;
+    
+    pos++;
+
+    int i;
+    long long boundry = 1;
+    for (i = 1; ; i++) {
+        long long new_boundry = (powll(base, i) - powll(base, i -1)) * i + boundry;
+        if (pos <= new_boundry) {
+            i--;
+            break;
+        }
+        boundry = new_boundry;
+    }
+    
+    if (i > 0) {
+        *result = powll(base, i);
+        pos -= boundry;
+    }
+
+    i++;
+    pos--;
+    *result += pos / i;
+    *digit = pos % i;
 }
 
 int main (void) {
@@ -56,20 +96,22 @@ int main (void) {
 
     while (1) {
         long long pos;
-        int sys;
-
-        if (input_pos(&pos, &sys))
-            return print_incorrect_input();
-
+        int base;
+        
+        int input_err = input_pos(&pos, &base);
+        
         if (clear_stdin())
             break;
 
+        if (input_err)
+            return print_incorrect_input();
+
         int digit;
-        int result[RESULT_LENGTH];
+        long long result;
 
-        solve(pos, sys, result, &digit);
+        solve(pos, base, &result, &digit);
 
-        print_result(result, digit);
+        print_result(result, base, digit);
     }
 
     return 0;
