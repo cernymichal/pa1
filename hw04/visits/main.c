38cd168ed9779ded75052506fb6cc9368c49808c
Michal Černý <33428111+cernymichal@users.noreply.github.com>
2021-11-08 01:27:57 +0100

    hw04: Move state to heap allocated struct

--- hw04/visits/main.c
+++ hw04/visits/main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define INPUT_LINE_LENGTH 256
+
 #define MAX_VISITS 1000000
+#define MAX_ID 99999
+#define ID_COUNT (MAX_ID + 1)
+
+typedef struct State_s {
+    int visits_total;
+    int visits_chrono[MAX_VISITS];
+    int client_visits[ID_COUNT];
+} State;
+
+State * new_state(void) {
+    State * state = (State *) malloc(sizeof(State));
+    state->visits_total = 0;
+
+    for (int i = 0; i < ID_COUNT; i++)
+        state->client_visits[i] = 0;
+
+    return state;
+} 
 
 void print_visit(int count) {
     if (count == 1)
@@ -19,76 +39,82 @@ int print_incorrect_input() {
     return 1;
 }
 
-int get_input(char * op, int * arg1, int * arg2, int visits) {
-    *op = 0;
-    *arg1 = *arg2 = -1;
-    
+int get_input(State * state, char * op, int * arg1, int * arg2) {
     char line[INPUT_LINE_LENGTH];
-    fgets(line, INPUT_LINE_LENGTH, stdin);
-    int scanned = sscanf(line, " %c %d %d", op, arg1, arg2);
-
-    if (scanned == EOF)
+    char * fgets_ret = fgets(line, INPUT_LINE_LENGTH, stdin);
+    
+    if (fgets_ret == NULL)
         return 2;
+
+    int scanned = sscanf(line, " %c %d %d", op, arg1, arg2);
 
     if (scanned < 1 || !(*op == '+' || *op == '?'))
         return 1;
 
-    if (*op == '+' && (scanned != 2 || *arg1 < 0 || *arg1 >= MAX_VISITS || visits >= MAX_VISITS))
+    if (*op == '+' && (scanned != 2 || *arg1 < 0 || *arg1 > MAX_ID || state->visits_total >= MAX_VISITS))
         return 1;
 
-    if (*op == '?' && (scanned != 3 || *arg1 < 0 || *arg2 >= visits || *arg1 > *arg2))
+    if (*op == '?' && (scanned != 3 || *arg1 < 0 || *arg2 >= state->visits_total || *arg1 > *arg2))
         return 1;
 
     return 0;
 }
 
-int add_visit(int * visits, int log[], int id) {
-    log[*visits] = id;
-    *visits += 1;
-
-    int count = 0;
-    for (int i = 0; i < *visits; i++) {
-        if (log[i] == id)
-            count++;
-    }
-
-    return count;
-}
-
-int count_unique_visits(int visits, int log[], int start, int end) {
-    return end - (start - 1);
-}
-
-int main (void) {
-    int visits = 0;
-    int log[MAX_VISITS];
-
-    printf("Pozadavky:\n");
+int add_visit(State * state, int id) {
+    state->visits_chrono[state->visits_total] = id;
+    state->visits_total++;
     
-    while (1) {
-        char operation;
-        int arg1, arg2;
-        
-        int input_result = get_input(&operation, &arg1, &arg2, visits);
+    state->client_visits[id]++;
 
+    return state->client_visits[id];
+}
+
+int count_unique_visits(State * state, int start, int end) {
+    int * clients = (int *) malloc(ID_COUNT * sizeof(int));
+
+    for (int i = 0; i < ID_COUNT; i++)
+        clients[i] = 0;
+
+    for (int i = start; i <= end; i++)
+        clients[state->visits_chrono[i]] += 1;
+
+    int unique = 0;
+    for (int i = 0; i < ID_COUNT; i++)
+        if (clients[i] > 0)
+            unique++;
+    
+    free(clients);
+
+    return unique;
+}
+
+int main_loop(State * state) {
+    printf("Pozadavky:\n");
+
+    while (1) {
+        char operation = 0;
+        int arg1 = -1, arg2 = -1;
+        
+        int input_result = get_input(state, &operation, &arg1, &arg2);
+        
         if (input_result == 1)
             return print_incorrect_input();
         else if (input_result == 2)
             break;
-
+        
         printf("> ");
         
         switch (operation) {
             case '+':
                 print_visit(
-                    add_visit(&visits, log, arg1)
-                );
+                    add_visit(state, arg1)
+                );    
                 break;
             case '?':
                 print_unique_visits(
-                    count_unique_visits(visits, log, arg1, arg2),
+                    count_unique_visits(state, arg1, arg2),
                     arg2 - (arg1 - 1)
-                );
+                );    
                 break;
             default:
                 return 1;
@@ -96,5 +122,15 @@ int main (void) {
     }
 
     return 0;
+}
+
+int main (void) {
+    State * state = new_state();
+
+    int return_code = main_loop(state);
+
+    free(state);
+
+    return return_code;
 }
 
