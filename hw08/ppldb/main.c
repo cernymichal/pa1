fc745b5d6e014835327f9c1c8a8d79e874161e46
Michal Černý <33428111+cernymichal@users.noreply.github.com>
2021-12-14 15:58:24 +0100

    hw08: Add VectorPerson

--- hw08/ppldb/main.c
+++ hw08/ppldb/main.c
@@ -5,6 +5,61 @@
 #include <stdlib.h>
 #include <string.h>
 
+#endif /* __PROGTEST__ */
+
+#define VECTOR_INITIAL_SIZE 10
+#define VECTOR_SIZE_FACTOR 2
+
+typedef struct Person_s {
+    int id;
+    char * name;
+    struct Person_s * ancestor1;
+    struct Person_s * ancestor2;
+} Person;
+
+// ============================== VectorPerson
+
+typedef struct VectorPerson_s {
+    Person * data;
+    int capacity;
+    int n;
+} VectorPerson;
+
+VectorPerson * vector_person_new() {
+    VectorPerson * vector = (VectorPerson *) malloc(sizeof(VectorPerson));
+
+    vector->capacity = VECTOR_INITIAL_SIZE;
+    vector->n = 0;
+    vector->data = (Person *) malloc(vector->capacity * sizeof(Person));
+
+    return vector;
+}
+
+void vector_person_free(VectorPerson * vector) {
+    if (!vector)
+        return;
+
+    free(vector->data);
+    free(vector);
+}
+
+void vector_person_push(VectorPerson * vector, char value) {
+    if (vector->n + 1 > vector->capacity) {
+        vector->capacity *= VECTOR_SIZE_FACTOR;
+        vector->data = (Person *) realloc(vector->data, vector->capacity * sizeof(Person));
+    }
+
+    vector->data[vector->n++] = value;
+}
+
+// /============================= VectorPerson
+
+typedef struct TDatabase {
+    VectorPerson * people
+} TDATABASE;
+
+#ifndef __PROGTEST__
+
 typedef struct TResult {
     struct TResult *m_Next;
     int m_ID;
@@ -12,10 +67,6 @@ typedef struct TResult {
 } TRESULT;
 
 #endif /* __PROGTEST__ */
-
-typedef struct TDatabase {
-    /* todo */
-} TDATABASE;
 
 void initAll(TDATABASE *db) {
     /* todo */
